137711210108e2928b928c77a558de3241a97ee4
Stocil <vashurov02@mail.ru>
2023-11-21 03:06:27 +0300

    test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 
 function App() {
-  return <h1>Vite + React + Stas = Hello World</h1>;
+  return <h1 className="stas">Vite + React + Stas = Hello World</h1>;
 }
 
 export default App;
