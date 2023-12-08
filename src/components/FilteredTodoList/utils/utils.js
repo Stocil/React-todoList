b5ee7e796e217ce279010cae1ca72f0eb04d1bcd
Stocil <vashurov02@mail.ru>
2023-12-08 22:00:18 +0300

    todo label fix

--- src/components/FilteredTodoList/utils/utils.js
+++ src/components/FilteredTodoList/utils/utils.js
@@ -7,30 +7,5 @@ export function getTodosFilteredBySearch(initialList, search) {
     return todo.task.toLowerCase().includes(search.toLowerCase());
   });
 
-  // const filteredTodos = initialList.filter((todo) => {
-  //   const filteredWord = todo.task
-  //     .toLowerCase()
-  //     .split(" ")
-  //     .map((word) => {
-  //       return word.startsWith(search);
-  //     });
-
-  //   const answer = isStartWith(filteredWord);
-
-  //   return answer;
-
-  //   function isStartWith(list) {
-  //     const array = list.filter((bools) => {
-  //       if (bools) return true;
-  //     });
-
-  //     if (array[0]) {
-  //       return true;
-  //     }
-
-  //     return false;
-  //   }
-  // });
-
   return filteredTodos;
 }
